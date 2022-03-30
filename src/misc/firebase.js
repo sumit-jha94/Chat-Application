8dd5af3f5fe9cb10e6800f4ce4edc08e71fb3d91
sumit-jha94 <sumitshine14@gmail.com>
2022-03-30 21:21:03 +0530

    created signin page and siginin with Google and Fb

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 // Taking the config file from Firebas>> Project Setting >>
 const config = {
@@ -13,4 +15,11 @@ const config = {
 
 // using this app varible we can use any serices of firebase
 const app = firebase.initializeApp(config);
+
+// exporting auth object to intereact with firebase.
+// It will give auth object to intereact with database
+export const auth = app.auth();
+
+// exporting database to use 
+export const database = app.database();
 
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Row, Col, Panel, Icon, Button, ButtonToolbar, Alert} from 'rsuite';
+import { auth, database } from '../misc/firebase';
+
 
 const SignIn = () => {
+
+  const signInWithProvider = async (provider) => {
+
+    try {
+      // Exporting the auth object from "misc >> firebase"
+        const {additionalUserInfo, user} = await auth.signInWithPopup(provider);
+        if(additionalUserInfo.isNewUser){
+        // using databse variable from "misc >> firebase and using the ref as path"
+        // Interacting with database is going to be a promise.  
+             await database.ref(`/profiles/${user.uid}`).set({
+              name: user.displayName,
+              createdAt : firebase.database.ServerValue.TIMESTAMP
+        })
+      }
+      Alert.success('Signed In', 4000)
+      } catch (err) {
+        Alert.info(err.message, 4000);
+    }
+    
+    
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider (new firebase.auth.FacebookAuthProvider())
+  } 
+
+  const onGoogleSignIn = () => {
+    signInWithProvider (new firebase.auth.GoogleAuthProvider())
+}
+
+
+
   return (
-    <div>SignIn</div>
+    <Container>
+      <Grid className='mt-page'>
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className='text-center'>
+                <h2>Welcome To SignIn Page</h2>
+                <p>Progressivs Chat App </p>
+              </div>
+              <div className='mt-3'>
+                <ButtonToolbar>
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                <Icon icon='facebook'/>Continue With FaceBook
+                </Button>
+
+                <Button block color="red" onClick={onGoogleSignIn}>
+                <Icon icon='google'/>Continue With Google
+                </Button>               
+               </ButtonToolbar>
+              </div>
+            </Panel>
+          
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
   )
 }
 
 export default SignIn
+// ReactDOM.render(instance);
