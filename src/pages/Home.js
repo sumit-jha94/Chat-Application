7e46f3d4246893a7fa2941476bd82163eecef4ab
sumit-jha94 <sumitshine14@gmail.com>
2022-04-01 23:24:38 +0530

    created sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Col, Grid, Row} from 'rsuite';
+import Sidebar from '../components/Sidebar';
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <Grid fluid className="h-100">
+    <Row>
+      <Col xs={24} md={8}>
+        <Sidebar />
+      </Col>
+    </Row>
+        </Grid>;
+      
+    };
